3c21e56843005659a7a01c4aee3603003aebdf56
S Rahul Badami <srahulbadami@Ss-MacBook-Pro.local>
2024-10-05 19:34:24 +0530

    Fixed Achievements for mobile and laptop

--- src/Components/Achievment.tsx
+++ src/Components/Achievment.tsx
@@ -2,6 +2,7 @@ import img1 from '../Assests/30.webp'
 import img2 from '../Assests/31.webp'
 import img3 from '../Assests/33.webp'
 import img4 from '../Assests/34.webp'
+import {isMobile, isTablet} from "../Utility";
 
 export function Achievment(){
     return (
@@ -9,7 +10,58 @@ export function Achievment(){
             <section className="achive-sec">
                 <div className="achive-container">
                     <h2 className="achive-header">Our Achivements</h2>
-                    <div className="achive-content">
+                    {isMobile() || isTablet() ?
+                        <div className="">
+
+                            <div className="">
+                                <div className="achive-container-mobile">
+                                    <img src={img1} className="weight-loss-icon" alt=""/>
+                                </div>
+
+                                <div className="achive-headings pt10">
+                                    <h3 className="h3-heading" data-target="5">5+</h3>
+                                    <p className="p-para">Year of Experience</p>
+                                </div>
+                            </div>
+                            <div style={{height: 40}}></div>
+                            <div className="">
+                                    <div className="achive-container-mobile">
+                                        <img src={img2} className="weight-loss-icon" alt=""/>
+                                    </div>
+
+                                    <div className="achive-headings pt10">
+                                        <h3 className="h3-heading" data-target="3000">3000+</h3>
+                                        <p className="p-para">Satisfied Customers</p>
+                                    </div>
+                                </div>
+
+                            <div style={{height: 40}}></div>
+                            <div className="">
+                                    <div className="achive-container-mobile">
+                                        <img src={img3} className="weight-loss-icon" alt=""/>
+                                    </div>
+
+                                    <div className="achive-headings pt10">
+                                        <h3 className="h3-heading" data-target="15000">15000+</h3>
+                                        <p className="p-para">Number of Kgs lost </p>
+                                    </div>
+                                </div>
+
+                            <div style={{height: 40}}></div>
+                            <div className="">
+                                <div className="achive-container-mobile">
+                                    <img src={img4} className="weight-loss-icon" alt=""/>
+                                </div>
+                                <div className="achive-headings pt10">
+                                    <h3 className="h3-heading" data-target="9">9+</h3>
+                                    <p className="p-para">Country</p>
+                                </div>
+                            </div>
+
+
+                        </div>:
+                        <div className="achive-content">
+
                         <div className="achive-wrapper">
                             <div className="achive-box">
                                 <div className="achive-icon">
@@ -55,7 +107,8 @@ export function Achievment(){
                             </div>
                         </div>
 
-                    </div>
+                    </div>}
+
                 </div>
 
             </section>
--- src/Utility.tsx
+++ src/Utility.tsx
@@ -2,6 +2,9 @@
 
 
 
- export const isMobile = () =>{
+export const isMobile = () =>{
     return window.innerWidth < 800;
+}
+export const isTablet = () =>{
+    return window.innerWidth > 800 && window.innerWidth < 1400;
 }
